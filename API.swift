ed0787c4f603146dc236240fde3202dce9f0a0bc
ziczacqb <hanhdinh@Hanh-Dinhs-MacBook-Pro.local>
2017-01-07 20:09:27 +0700

    [ADD] API.swift
    them API

---
+++ API.swift
@@ -0,0 +1,22 @@
+//
+//  API.swift
+//  appBanHang
+//
+//
+
+import Foundation
+import UIKit
+
+enum API:String{
+    case postImage = "iosuploadImage.php"
+    case getSP = "GETSP.php"
+    case dataJson = "data.json"
+    case getCata = "Cata.php"
+    //case postImage = "PostImage.php"
+    var fullLink:String{
+        return "http://hidaacademy.com/" + self.rawValue
+    }
+}
+
+
+
